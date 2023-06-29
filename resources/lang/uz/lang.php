6e8af7a2eb3ce3e542c0dd2a75aee37c5ca9be51
Shohruh88 <shohruh.8899@mail.ru>
2023-06-29 20:15:04 +0500

    translate en ru

--- resources/lang/uz/lang.php
+++ resources/lang/uz/lang.php
@@ -13,7 +13,23 @@ return [
     'Why_Choose_Us' => "Nega bizni tanlaysiz",
     'watch_video' => "Videoni tomosha qiling",
     'Boshlash' => "Boshlash",
-
+    'abd' => "Biroz qochish yoki xozr qo'ng'iroq qilish",
+    'expertlar' =>"Bizning expertlar bilan xoziroq bog'laning",
+    'boglanish' => "Harakatga chaqirish",
+    'malakali_yurist' => "Malakali yurist maslahati zarur vaziyatga duch keldingizmi?
+    Qayerdan yurist topishni bilmayapsiz yoki konsultatsiyaga borishga vaqtingiz yo'qmi?
+    Normativ hujjatlardan javob topishga harakat qildingiz ammo amaliy tajribaga mos tushadigan javob topaolmadingizmi?",
+    'malakali' => "  Malakali yuristlar xizmatidan onlayn foydalaning. Xizmatlar kafolatlanadi.
+    Hoziroq savol bering, O'zbekistonning eng yaxshi yurist va advokatlari javob beradilar.",
+    'bizning_jamoa_haqida' => "Bizning jamoa haqida",
+    'kunduzgi_yordam' => "Kunduzi yuridik yordam",
+    'Shaxsiy_advokat.' => "Shaxsiy advokat.",
+    'huquqiy_taxlil' => "",
+    '' => "",
+    '' => "",
+    '' => "",
+    '' => "",
+    '' => "",
    //contact
     'location' => "Manzili",
 
